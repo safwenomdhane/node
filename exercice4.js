f8f5070e3a17a9d6b1d7919e429c7f7132dab35f
safwenomdhane <85678154+safwenomdhane@users.noreply.github.com>
2021-09-18 09:53:31 +0100

    Add files via upload

---
+++ exercice4.js
@@ -0,0 +1,6 @@
+var fs = require('fs')  
+var file = process.argv[2]  
+fs.readFile(file,'utf-8', function (err, contents) {  
+  var lines = contents.split('\n').length-1  
+  console.log(lines)  
+})
